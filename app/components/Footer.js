261503b5cd8bfb9e280087ae53f5585a4210b618
Rafat Parween <109865229+rafatparween@users.noreply.github.com>
2025-06-14 00:12:05 +0530

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="container mx-auto px-20 lg:px-20 py-20 flex flex-col gap-10 md:flex-row justify-between border-t border-slate-800">
                 <div className="flex">
                     <div className="font-bold text-center">
-                        <h1 className="text-[32px] mt-[89px] font-bold text-purple-300 cursor-pointer">Micro-Sonic</h1>
+                        <h1 className="text-[32px] mt-[89px] font-bold text-purple-300 cursor-pointer">123hpeasyprintsolutions</h1>
                     </div>
                 </div>
 
@@ -76,7 +76,7 @@ const Footer = () => {
             </div>
 
             <div className="flex items-center justify-center py-10 mt-[-103px]">
-                <span className="text-gray-400 leading-10">© Copyright, All Rights Reserved by Micro-Sonic</span>
+                <span className="text-gray-400 leading-10">© Copyright, All Rights Reserved by 123hpeasyprintsolutions</span>
             </div>
         </footer>
     );
